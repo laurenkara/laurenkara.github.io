05aeaccc7aa16383c97b7a30b908dd11aeda9752
laurenkara <50687281+laurenkara@users.noreply.github.com>
2020-11-11 13:12:48 +0800

    brutalist web trend

--- js/main.js
+++ js/main.js
@@ -1,45 +1,14 @@
-// code to hover on an element and add a class to another element
-//   $(document).ready(function() {     
-//     $('#info').hover(function(){     
-//         $('#title').addClass('rumble');    
-//     },     
-//     function(){    
-//         $('#title').removeClass('rumble');     
-//     });
-// });   
+//auto collapse navbar on mobile
+$('.navbar-collapse a').click(function(){
+    $(".navbar-collapse").collapse('hide');
+});
 
-// //customer cursor
-// const $bigBall = document.querySelector('.cursor__ball--big');
-// const $smallBall = document.querySelector('.cursor__ball--small');
-// const $hoverables = document.querySelectorAll('.hoverable');
-
-// // Listeners
-// document.body.addEventListener('mousemove', onMouseMove);
-// for (let i = 0; i < $hoverables.length; i++) {
-//   $hoverables[i].addEventListener('mouseenter', onMouseHover);
-//   $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
-// }
-
-// // Move the cursor
-// function onMouseMove(e) {
-//   TweenMax.to($bigBall, .4, {
-//     x: e.pageX - 15,
-//     y: e.pageY - 15
-//   })
-//   TweenMax.to($smallBall, .1, {
-//     x: e.pageX - 5,
-//     y: e.pageY - 7
-//   })
-// }
-
-// // Hover an element
-// function onMouseHover() {
-//   TweenMax.to($bigBall, .3, {
-//     scale: 4
-//   })
-// }
-// function onMouseHoverOut() {
-//   TweenMax.to($bigBall, .3, {
-//     scale: 1
-//   })
-// }
+//masonry gallery
+$(".grid").imagesLoaded(function() {
+    $(".grid").masonry({
+      itemSelector: ".grid-item"
+    });
+  });
+  
+  // curosr 
+  new emojiCursor({emoji: ["💖", "🦋", "🌈", "🌻"]});
